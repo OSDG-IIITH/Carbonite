53f7d86ec745d3028ba88f9c5c7c528aa317fdf0
varshitakolipaka <varshita2003@gmail.com>
2021-08-22 16:16:29 +0400

    Added Language Specification Feature

--- carbon.js
+++ carbon.js
@@ -1,38 +1,131 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-const getImageFile = async (code) => {
+const getImageFile = async (code, language) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const browser = await puppeteer.launch();
+			const carbonPage = await browser.newPage({
+				waitUntil: "networkidle0",
+				timeout: 0,
+			});
 
-    return new Promise(async (resolve, reject) => {
+			await carbonPage.setViewport({
+				width: 1920,
+				height: 1080,
+				deviceScaleFactor: 2.5,
+			});
 
-        try {
-            const browser = await puppeteer.launch();
-            const carbonPage = await browser.newPage({ waitUntil: 'networkidle0', timeout: 0 });
-            await carbonPage.setViewport({
-                width: 1920,
-                height: 1080,
-                deviceScaleFactor: 2.5
-            });
+			code = encodeURIComponent(code);
+			link = `https://carbon.now.sh/?code=${code}&l=${language}`;
 
-            await carbonPage.goto(`https://carbon.now.sh/?code=${encodeURIComponent(code)}`);
-            await carbonPage.waitForSelector('#export-container');
-            const element = await carbonPage.$('#export-container');
+			await carbonPage.goto(link);
 
-            const carbonPic = await element.screenshot({
-                type: 'jpeg',
-                quality: 100,
-            });
+			await carbonPage.waitForSelector("#export-container");
+			const element = await carbonPage.$("#export-container");
 
-            await carbonPage.close();
-            await browser.close();
+			const carbonPic = await element.screenshot({
+				type: "jpeg",
+				quality: 100,
+			});
 
-            resolve(carbonPic);
+			await carbonPage.close();
+			await browser.close();
 
-        }
-        catch (e) {
-            reject(e);
-        }
+			resolve(carbonPic);
+		} catch (e) {
+			reject(e);
+		}
+	});
+};
 
-    });
-}
+const checkLanguage = async (language) => {
+	languages = {
+		auto: "auto",
+		apache: "text%2Fapache",
+		bash: "application%2Fx-sh",
+		plaintext: "text",
+		c: "text%2Fx-csrc",
+		"c++": "text%2Fx-c%2B%2Bsrc",
+		cpp: "text%2Fx-c%2B%2Bsrc",
+		"c#": "text%2Fx-csharp",
+		clojure: "clojure",
+		cobol: "cobol",
+		coffeeScript: "coffeescript",
+		crystal: "crystal",
+		css: "css",
+		d: "d",
+		dart: "dart",
+		diff: "text%2Fx-diff",
+		django: "django",
+		docker: "dockerfile",
+		elixir: "elixir",
+		elm: "elm",
+		erlang: "erlang",
+		fortran: "fortran",
+		gherkin: "gherkin",
+		graphQL: "graphql",
+		go: "text%2Fx-go",
+		groovy: "groovy",
+		handlebars: "handlebars",
+		haskell: "haskell",
+		html: "htmlmixed",
+		xml: "htmlmixed",
+		java: "text%2Fx-java",
+		javascript: "javascript",
+		js: "javascript",
+		json: "application%2Fjson",
+		jsx: "jsx",
+		julia: "julia",
+		kotlin: "text%2Fx-kotlin",
+		latex: "stex",
+		lisp: "commonlisp",
+		lua: "lua",
+		markdown: "markdown",
+		md: "markdown",
+		mathematica: "mathematica",
+		matlab: "text%2Fx-octave",
+		octave: "text%2Fx-octave",
+		mysql: "text%2Fx-mysql",
+		"n-triples": "application%2Fn-triples",
+		nginx: "nginx",
+		nim: "nim",
+		"objective-c": "text%2Fx-objectivec",
+		"ocaml/F#": "mllike",
+		"f#": "mllike",
+		pascal: "pascal",
+		perl: "perl",
+		php: "text%2Fx-php",
+		powershell: "powershell",
+		python: "python",
+		py: "python",
+		r: "r",
+		"risc-v": "riscv",
+		ruby: "ruby",
+		rust: "rust",
+		sass: "sass",
+		scala: "text%2Fx-scala",
+		smalltalk: "smalltalk",
+		solidity: "solidity",
+		sparql: "application%2Fsparql-query",
+		sql: "sql",
+		stylus: "stylus",
+		swift: "swift",
+		tcl: "tcl",
+		toml: "toml",
+		turtle: "text%2Fturtle",
+		typescript: "application%2Ftypescript",
+		tsx: "text%2Ftypescript-jsx",
+		twig: "text%2Fx-twig",
+		"vb-net": "vb",
+		Verilog: "verilog",
+		VHDL: "vhdl",
+		Vue: "vue",
+		XQuery: "xquery",
+		YAML: "yaml",
+	};
 
-module.exports = { getImageFile };
+	if (languages[language]) return languages[language];
+	else return "auto";
+};
+
+module.exports = { getImageFile, checkLanguage };
--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const Discord = require('discord.js')
-const { getImageFile } = require('./carbon');
-require('dotenv').config()
+const Discord = require("discord.js");
+const { getImageFile, checkLanguage } = require("./carbon");
+require("dotenv").config();
 
 const client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
+	intents: [
+		Discord.Intents.FLAGS.GUILDS,
+		Discord.Intents.FLAGS.GUILD_MESSAGES,
+	],
 });
 
-client.on('ready', () => {
-    console.log(`Carbonite is active as ${client.user.tag}`);
+client.on("ready", () => {
+	console.log(`Carbonite is active as ${client.user.tag}`);
 });
 
-client.on('messageCreate', async msg => {
-    if (msg.content.startsWith('carbonite')) {
-        let mdCode = msg.content.substring(msg.content.indexOf('\n') + 1);
-        if (mdCode.startsWith('```\n') && mdCode.endsWith('\n```')) {
-            mdCode = mdCode.replace('```\n', '');
-            mdCode = mdCode.replace('\n```', '');
-            const codeImage = await getImageFile(mdCode);
-            msg.channel.send({ files: [{ attachment: codeImage }] });
-        }
-        else
-            msg.reply("Invalid syntax. Could not parse message.");
-    }
+client.on("messageCreate", async (msg) => {
+	if (msg.content.startsWith("carbonite")) {
+		let mdCode = msg.content.substring(msg.content.indexOf("\n") + 1);
+		// parses out the language specified in ```language
+		let mdLanguage = mdCode.split(" ")[0].split("\n")[0].substring(3);
+		// first word in code (specifies language, and beginning of code)
+		let language = await checkLanguage(mdLanguage);
+
+		if (
+			mdCode.startsWith(`\`\`\`${mdLanguage}\n`) &&
+			mdCode.endsWith("\n```")
+		) {
+			// remove the words with ``` from the code
+			mdCode = mdCode.replace(`\`\`\`${mdLanguage}\n`, "");
+			mdCode = mdCode.replace("\n```", "");
+
+			const codeImage = await getImageFile(mdCode, language);
+			msg.channel.send({ files: [{ attachment: codeImage }] });
+		} else msg.reply("Invalid syntax. Could not parse message.");
+	}
 });
 
 client.login(process.env.DISCORD_TOKEN);
