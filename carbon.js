92d77b9e9ce2316cd77a0e31ae7951a5ac6020e8
Freyam Mehta <frey77mehta@gmail.com>
2021-08-22 15:19:14 +0400

    formatted code

--- carbon.js
+++ carbon.js
@@ -1,73 +1,75 @@
 const puppeteer = require("puppeteer");
 
 const getImageFile = async (code, theme) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const browser = await puppeteer.launch();
-      const carbonPage = await browser.newPage({
-        waitUntil: "networkidle0",
-        timeout: 0,
-      });
-      await carbonPage.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 2.5,
-      });
+    return new Promise(async (resolve, reject) => {
+        try {
+            const browser = await puppeteer.launch();
+            const carbonPage = await browser.newPage({
+                waitUntil: "networkidle0",
+                timeout: 0,
+            });
+            await carbonPage.setViewport({
+                width: 1920,
+                height: 1080,
+                deviceScaleFactor: 2.5,
+            });
 
-      await carbonPage.goto(
-        `https://carbon.now.sh/?code=${encodeURIComponent(code)}&t=${theme}`
-      );
-      await carbonPage.waitForSelector("#export-container");
-      const element = await carbonPage.$("#export-container");
+            await carbonPage.goto(
+                `https://carbon.now.sh/?code=${encodeURIComponent(
+                    code
+                )}&t=${theme}`
+            );
+            await carbonPage.waitForSelector("#export-container");
+            const element = await carbonPage.$("#export-container");
 
-      const carbonPic = await element.screenshot({
-        type: "jpeg",
-        quality: 100,
-      });
+            const carbonPic = await element.screenshot({
+                type: "jpeg",
+                quality: 100,
+            });
 
-      await carbonPage.close();
-      await browser.close();
+            await carbonPage.close();
+            await browser.close();
 
-      resolve(carbonPic);
-    } catch (e) {
-      reject(e);
-    }
-  });
+            resolve(carbonPic);
+        } catch (e) {
+            reject(e);
+        }
+    });
 };
 
 const checkTheme = async (theme) => {
-  const themes = [
-    "3024-night",
-    "a11y-dark",
-    "base16-dark",
-    "base16-light",
-    "blackboard",
-    "cobalt",
-    "duotone-dark",
-    "hopscotch",
-    "lucario",
-    "material",
-    "monokai",
-    "night-owl",
-    "nord",
-    "oceanic-next",
-    "one-light",
-    "one-dark",
-    "panda-syntax",
-    "paraiso-dark",
-    "seti",
-    "shades-of-purple",
-    "solarized+dark",
-    "solarized+light",
-    "synthwave-84",
-    "twilight",
-    "verminal",
-    "vscode",
-    "yeti",
-    "zenburn",
-  ];
+    const themes = [
+        "3024-night",
+        "a11y-dark",
+        "base16-dark",
+        "base16-light",
+        "blackboard",
+        "cobalt",
+        "duotone-dark",
+        "hopscotch",
+        "lucario",
+        "material",
+        "monokai",
+        "night-owl",
+        "nord",
+        "oceanic-next",
+        "one-light",
+        "one-dark",
+        "panda-syntax",
+        "paraiso-dark",
+        "seti",
+        "shades-of-purple",
+        "solarized+dark",
+        "solarized+light",
+        "synthwave-84",
+        "twilight",
+        "verminal",
+        "vscode",
+        "yeti",
+        "zenburn",
+    ];
 
-  return themes.includes(theme);
+    return themes.includes(theme);
 };
 
 module.exports = { getImageFile, checkTheme };
--- index.js
+++ index.js
@@ -3,27 +3,30 @@ const { getImageFile, checkTheme } = require("./carbon");
 require("dotenv").config();
 
 const client = new Discord.Client({
-  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+    ],
 });
 
 client.on("ready", () => {
-  console.log(`Carbonite is active as ${client.user.tag}`);
+    console.log(`Carbonite is active as ${client.user.tag}`);
 });
 
 client.on("messageCreate", async (msg) => {
-  if (msg.content.startsWith("carbonite")) {
-    let theme = msg.content.match(/\S+/gi)[1];
+    if (msg.content.startsWith("carbonite")) {
+        let theme = msg.content.match(/\S+/gi)[1];
 
-    theme = (await checkTheme(theme)) ? theme : "nord";
+        theme = (await checkTheme(theme)) ? theme : "nord";
 
-    let mdCode = msg.content.substring(msg.content.indexOf("\n") + 1);
-    if (mdCode.startsWith("```\n") && mdCode.endsWith("\n```")) {
-      mdCode = mdCode.replace("```\n", "").replace("\n```", "");
+        let mdCode = msg.content.substring(msg.content.indexOf("\n") + 1);
+        if (mdCode.startsWith("```\n") && mdCode.endsWith("\n```")) {
+            mdCode = mdCode.replace("```\n", "").replace("\n```", "");
 
-      const codeImage = await getImageFile(mdCode, theme);
-      msg.channel.send({ files: [{ attachment: codeImage }] });
-    } else msg.reply("Invalid syntax. Could not parse message.");
-  }
+            const codeImage = await getImageFile(mdCode, theme);
+            msg.channel.send({ files: [{ attachment: codeImage }] });
+        } else msg.reply("Invalid syntax. Could not parse message.");
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
